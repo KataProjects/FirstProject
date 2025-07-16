6cf0b86433e5c83907ce68de941889e6e7661529
Seva <20211206nch@mail.ru>
2025-07-16 15:43:29 +0300

    created a Table component and styled it

---
+++ src/features/table/ui/Table.tsx
@@ -0,0 +1,14 @@
+import { Table, type TableProps } from 'antd';
+
+import styles from './Table.module.css';
+
+export const MyTable = <T extends object = any>({
+  pagination = { position: ['bottomLeft'], showSizeChanger: false },
+  ...props
+}: TableProps<T>) => {
+  return (
+    <div className={styles.tableWrapper}>
+      <Table<T> {...props} className={styles.customTable} pagination={pagination} />
+    </div>
+  );
+};
