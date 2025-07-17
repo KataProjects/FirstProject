6f4edaa6ef6c783bbe4ae278d3a2720f344ade9c
Ildus Rizvanov <ildusrizvanov@yandex.ru>
2025-07-17 15:50:09 +0300

    Установил SASS, antd, сделал вложенную структуру

---
+++ src/features/SignInForm/ui/SignInForm.tsx
@@ -0,0 +1,9 @@
+import { Input } from 'antd';
+
+const SignInForm: React.FC = () => {
+  return (
+      <Input.Password placeholder="input password" />
+  );
+};
+
+export { SignInForm }
--- src/pages/signIn/SignIn.tsx
+++ src/pages/signIn/SignIn.tsx
@@ -1,3 +1,12 @@
+import { SignInForm } from '@features';
+import styles from "./SignIn.module.scss"
+
 export const SignIn = () => {
-  return <div>SignIn</div>;
+   return (
+    <div className={styles.signInBox}>
+      <h1>Вход</h1>
+      <SignInForm />
+    </div>
+   )
+
 };
