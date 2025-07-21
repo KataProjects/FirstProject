8a3f47902742759f82b210e4754310cef7de6783
Seva <20211206nch@mail.ru>
2025-07-21 19:22:03 +0300

    added route /aircrafts

--- src/app/routes/navRoutes.tsx
+++ src/app/routes/navRoutes.tsx
@@ -1,14 +1,15 @@
+import type { RoutesProps } from '@app/types';
+import { TableAircraftPage } from '@pages/tableAircraft';
 import { HomePage } from '@pages/homePage';
+import { NotFoundPage } from '@pages/notFoundPage';
 import { SignIn } from '@pages/signIn';
 import { SignUp } from '@pages/signUp';
-import { NotFoundPage } from '@pages/notFoundPage';
-
-import type { RoutesProps } from '@app/types';
 
 export const ROUTES = {
   HOME: '/',
   SIGN_UP: '/sign_up',
   SIGN_IN: '/sign_in',
+  TABLE_PLANE: '/aircrafts',
   NOT_FOUND: '*',
 };
 
@@ -16,5 +17,6 @@ export const navRoutes: RoutesProps = [
   { path: ROUTES.HOME, element: <HomePage /> },
   { path: ROUTES.SIGN_IN, element: <SignIn /> },
   { path: ROUTES.SIGN_UP, element: <SignUp /> },
+  { path: ROUTES.TABLE_PLANE, element: <TableAircraftPage /> },
   { path: ROUTES.NOT_FOUND, element: <NotFoundPage /> },
 ];
