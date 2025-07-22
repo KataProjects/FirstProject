5b7b47b1a46746aa7a42ea021c00b2e1e7ce7507
Andrey <leprekon007@gmail.com>
2025-07-22 19:35:51 +0300

    added admin/timezones plus routes

--- src/app/routes/navRoutes.tsx
+++ src/app/routes/navRoutes.tsx
@@ -2,6 +2,7 @@ import { HomePage } from '@pages/homePage';
 import { SignIn } from '@pages/signIn';
 import { SignUp } from '@pages/signUp';
 import { NotFoundPage } from '@pages/notFoundPage';
+import { TimeZonesPage } from '@pages/admin/timeZones/timeZonesPage';
 
 import type { RoutesProps } from '@app/types';
 
@@ -10,6 +11,7 @@ export const ROUTES = {
   SIGN_UP: '/sign_up',
   SIGN_IN: '/sign_in',
   NOT_FOUND: '*',
+  TIMEZONES: 'admin/timezones',
 };
 
 export const navRoutes: RoutesProps = [
@@ -17,4 +19,5 @@ export const navRoutes: RoutesProps = [
   { path: ROUTES.SIGN_IN, element: <SignIn /> },
   { path: ROUTES.SIGN_UP, element: <SignUp /> },
   { path: ROUTES.NOT_FOUND, element: <NotFoundPage /> },
+  { path: ROUTES.TIMEZONES, element: <TimeZonesPage /> }
 ];
--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -6,7 +6,7 @@ import { Layout } from '@widgets/layouts';
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path={ROUTES.HOME} element={<Layout/>}>
+      <Route path={ROUTES.HOME} element={<Layout />}>
         {navRoutes.map((route) => (
           <Route key={route.path} path={route.path} element={route.element} />
         ))}
