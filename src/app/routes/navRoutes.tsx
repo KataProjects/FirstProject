2e033468730679234fddd4b04d5809eab3205dfe
Ildus Rizvanov <ildusrizvanov@yandex.ru>
2025-07-27 15:59:52 +0300

    Воспользовался готовыми компонентам Table и TableHeader

--- src/app/routes/navRoutes.tsx
+++ src/app/routes/navRoutes.tsx
@@ -1,14 +1,15 @@
+import type { RoutesProps } from '@app/types';
+import { TableDestinationPage } from '@pages/admin/tableDestination';
 import { HomePage } from '@pages/homePage';
+import { NotFoundPage } from '@pages/notFoundPage';
 import { SignIn } from '@pages/signIn';
 import { SignUp } from '@pages/signUp';
-import { NotFoundPage } from '@pages/notFoundPage';
-
-import type { RoutesProps } from '@app/types';
 
 export const ROUTES = {
   HOME: '/',
   SIGN_UP: '/sign_up',
   SIGN_IN: '/sign_in',
+  TABLE_DESTINATION: '/admin/destinations',
   NOT_FOUND: '*',
 };
 
@@ -16,5 +17,6 @@ export const navRoutes: RoutesProps = [
   { path: ROUTES.HOME, element: <HomePage /> },
   { path: ROUTES.SIGN_IN, element: <SignIn /> },
   { path: ROUTES.SIGN_UP, element: <SignUp /> },
+  { path: ROUTES.TABLE_DESTINATION, element: <TableDestinationPage /> },
   { path: ROUTES.NOT_FOUND, element: <NotFoundPage /> },
 ];
--- src/shared/ui/table/ui/Table.tsx
+++ src/shared/ui/table/ui/Table.tsx
@@ -3,7 +3,7 @@ import { Table, type TableProps } from 'antd';
 import styles from './Table.module.scss';
 
 export const MyTable = <T extends object = any>({
-  pagination = { position: ['bottomLeft'], showSizeChanger: false },
+  pagination = { position: ['bottomLeft'], showSizeChanger: true },
   ...props
 }: TableProps<T>) => {
   return (
