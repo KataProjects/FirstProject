4b500ff0c8a931da5ab04801d7198760149ee84f
Andrey <leprekon007@gmail.com>
2025-08-05 11:29:45 +0300

    AddButton теперь полностью универсален

--- src/shared/ui/AddButton/AddButton.tsx
+++ src/shared/ui/AddButton/AddButton.tsx
@@ -1,20 +1,17 @@
 import { PlusOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
+import { Button, type ButtonProps } from 'antd';
 import { type FC } from 'react';
 
-interface AddButtonProps {
+interface AddButtonProps extends ButtonProps {
   text: string;
-  onClick: () => void;
-  className?: string;
 }
 
-export const AddButton: FC<AddButtonProps> = ({ text, onClick, className }) => {
+export const AddButton: FC<AddButtonProps> = ({ text, ...rest }) => {
   return (
     <Button
       type="primary"
       icon={<PlusOutlined style={{ marginRight: '8px' }} />}
-      onClick={onClick}
-      className={className}
+      {...rest}
     >
       {text}
     </Button>
