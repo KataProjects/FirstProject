b652c1209c7e5addd78b802bd1e0b6362172c755
Elvira Elshina <elvircikfrolenochka@gmail.com>
2025-08-05 15:42:47 +0700

    adding mock

--- src/pages/admin/tableSeats/tableSeats.tsx
+++ src/pages/admin/tableSeats/tableSeats.tsx
@@ -1,5 +1,27 @@
 import { useEffect } from "react";
+import { Table } from "@shared/ui/table/index";
 
 export const TableSeatsPage = () => {
+    interface ITableSeats {
+        id: number;
+        name: string;
+        description: string;
+        createdAt: string;
+        updatedAt: string;
+    }
 
+    const columns: ITableSeats[] = [
+        {    id: 1,
+            name: 'string',
+            description: 'string',
+            createdAt: 'string',
+            updatedAt: 'string',
+        },
+        
+    ]
+return (
+    <div>
+        <Table dataSource={[]} columns={columns} />
+    </div>
+)
 }
