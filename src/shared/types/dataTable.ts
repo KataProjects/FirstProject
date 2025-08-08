011f3b2095615b4dd41121b43b378a11143bbbcc
Elvira Elshina <elvircikfrolenochka@gmail.com>
2025-08-08 14:23:32 +0700

    bug fix 3 commit

--- src/shared/types/dataTable.ts
+++ src/shared/types/dataTable.ts
@@ -92,11 +92,4 @@ export interface IContentDestinationTable {
   airportName: string;
 }
 
-export interface bookingData {
-  id: number,
-  passengerId: number,
-  bookingDate: string,
-  bookingStatus: string,
-  flightSeatId: number,
-  flightId: number
-}
+
